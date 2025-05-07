7a7f4d0e769f59122c1b263619c57328c9cdb88a
kkuratolodon <mif05123@gmail.com>
2025-05-07 21:41:23 +0700

    benerin 1 line buat hilmy

--- Scripts/dynamic_tiles.gd
+++ Scripts/dynamic_tiles.gd
@@ -2,35 +2,36 @@ extends TileMapLayer
 class_name DynamicTiles
 
 @export var TILE_SCENES := {
-	1: preload("res://Scenes/walkable_tile.tscn"),
-	2: preload("res://Scenes/border_tile.tscn")	
+    1: preload("res://Scenes/walkable_tile.tscn"),
+    2: preload("res://Scenes/border_tile.tscn")	
 }
 
 @onready var half_cell_size := tile_set.tile_size / 2
 
 func _ready():
-	await get_tree().process_frame
-	replace_tiles_with_scenes()
+    await get_tree().process_frame
+    replace_tiles_with_scenes()
 
 func replace_tiles_with_scenes(scene_dictionary: Dictionary = TILE_SCENES):
-	for tile_pos in get_used_cells():
-		var tile_data := get_cell_tile_data(tile_pos)
-		var source_id := get_cell_source_id(tile_pos)
-		
-		if scene_dictionary.has(source_id):
-			var object_scene = scene_dictionary[source_id]
-			replace_tile_with_object(tile_pos, object_scene)
+    for tile_pos in get_used_cells():
+        var tile_data := get_cell_tile_data(tile_pos)
+        var source_id := get_cell_source_id(tile_pos)
+        
+        if scene_dictionary.has(source_id):
+            var object_scene = scene_dictionary[source_id]
+            replace_tile_with_object(tile_pos, object_scene)
 
 func replace_tile_with_object(tile_pos: Vector2i, object_scene: PackedScene, parent: Node = null):
-	if get_cell_source_id(tile_pos) != -1:
-		set_cell(tile_pos, -1)
-	
-	if object_scene:
-		var obj = object_scene.instantiate()
-		var ob_pos = map_to_local(tile_pos) + Vector2(half_cell_size)
-		
-		if parent == null:
-			parent = get_tree().current_scene
-			
-		parent.add_child(obj)
-		obj.global_position = to_global(ob_pos)
+    print(tile_pos)
+    if get_cell_source_id(tile_pos) != -1:
+        set_cell(tile_pos, -1)
+    
+    if object_scene:
+        var obj = object_scene.instantiate()
+        var ob_pos = map_to_local(tile_pos)
+        
+        if parent == null:
+            parent = get_tree().current_scene
+            
+        parent.add_child(obj)
+        obj.global_position = to_global(ob_pos)
