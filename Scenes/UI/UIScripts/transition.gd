31d856e456e1726d184c28bc752f66d9e8e91c0b
MRadhityaUtomo <radhityautomo26@gmail.com>
2025-05-22 09:47:26 +0700

    transition

--- Scenes/UI/UIScripts/transition.gd
+++ Scenes/UI/UIScripts/transition.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 func change_scene_to_file(target: String) -> void:
 	print("Transitioning to scene: " + target)
-	$AnimationPlayer.play("dissolve")
-	await $AnimationPlayer.animation_finished
+	#$AnimationPlayer.play("dissolve")
+	#await $AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file(target)
-	$AnimationPlayer.play_backwards("dissolve")
+	#$AnimationPlayer.play_backwards("dissolve")
